d1b69d917c746662b908ee5fe6ce0c7b50d4c5cf
Craig Campbell <craigaaroncampbell@gmail.com>
2016-08-03 00:46:33 -0700

    add duplicate passion warning

--- app/js/dashboard/dashboard_controller.js
+++ app/js/dashboard/dashboard_controller.js
@@ -2,6 +2,7 @@ const URL = require(__dirname + '/../config');
 
 module.exports = function(app) {
   app.controller('DashboardController', ['$http', '$location', function($http, $location) {
+
     this.getUserData = function() {
       $http.defaults.headers.common.Authorization = 'Bearer ' +  window.localStorage.getItem('token');
       $http.get(URL.baseUrl + '/user_info')
@@ -20,18 +21,24 @@ module.exports = function(app) {
           console.log('some sort of error');
           console.log(err);
         });
-        this.clearInputs();
+      this.clearInputs();
     };
 
-    this.updatePassions = function(passions) {
-      $http.post(URL.baseUrl + '/passions', JSON.stringify({'passions': [passions]}))
-        .then((res) => {
-          this.userData = res.data.user;
-        }, (err) => {
-          console.log('some sort of error');
-          console.log(err);
-        });
-        this.clearInputs();
+    this.updatePassions = function(passion) {
+      if(this.passionExists(passion)) {
+          this.duplicatePassion = true;
+          this.duplicatePassionName = passion.name;
+        } else {
+          $http.post(URL.baseUrl + '/passions', JSON.stringify({'passions': [passion]}))
+          .then((res) => {
+            this.userData = res.data.user;
+            if (this.duplicatePassion) this.toggleDuplicatePassion();
+          }, (err) => {
+            console.log('some sort of error');
+            console.log(err);
+          });
+        }
+      this.clearInputs();
     };
 
     this.signOut = function() {
@@ -45,6 +52,18 @@ module.exports = function(app) {
         inputTags[i].value = null;
         inputTags[i].checked = false;
       }
+    };
+
+    this.passionExists = function(passion) {
+      for (var i = 0; i < this.userData.passions.length; i++) {
+        if (passion.name === this.userData.passions[i].name) return true;
+      }
+      return false;
+    };
+
+    this.toggleDuplicatePassion = function() {
+      this.duplicatePassion = !this.duplicatePassion;
+      this.duplicatePassionName = '';
     };
 
   }]);
